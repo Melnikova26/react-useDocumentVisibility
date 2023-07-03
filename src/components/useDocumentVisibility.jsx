7d42938196c27c5679fcc9984968b167a956207c
Melnikova26 <mmelnekowa@gmail.com>
2023-07-03 21:58:29 +0500

    feat: add useDocumentVisibility hook

---
+++ src/components/useDocumentVisibility.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect, useCallback } from "react";
+
+export function useDocumentVisibility() {
+    const [count, setCount] = useState(0);
+    const [visible, setVisible] = useState(true);
+    const changeState = () => {
+        let visibilityState = document.visibilityState;
+        if (visibilityState === 'visible'){
+            setVisible(true);
+        } else {
+            setVisible(false);
+            setCount(prevCount => prevCount + 1);
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener('visibilitychange', changeState);
+        return () => document.removeEventListener('visibilitychange', changeState);
+    }, []);
+
+    const onVisibilityChange = useCallback((isVisible) => {
+        const getMessage = () => isVisible(document.visibilityState === 'visible');
+        document.addEventListener('visibilitychange', getMessage);
+        return () => document.removeEventListener('visibilitychange', getMessage);
+    }, [visible]);
+
+    return {
+        count,
+        visible,
+        onVisibilityChange
+    }
+}
